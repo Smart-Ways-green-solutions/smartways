a814cc1b37ed8a11549eb544f7296ee90afbf13a
ms <ms@indigo7.de>
2024-02-24 22:27:27 +0100

    icons

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-import './fonts/fontawesome/css/all.css'
+// import './fonts/fontawesome/css/all.css'
 
 //const $ = require('jquery');
 //window.jQuery = $;
@@ -8,6 +8,7 @@ import './fonts/fontawesome/css/all.css'
 
 // import * as bootstrap from 'bootstrap';
 import 'bootstrap';
+import 'bootstrap-icons'
 
 //import toast from 'bootstrap/js/src/toast';
 //window.toast = toast;
