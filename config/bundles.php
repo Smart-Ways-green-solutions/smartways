9fbb830ac5ac292cfcdead0f8c1e47ea55bdf889
Michael Silberbauer <ms@indigo7.de>
2024-05-13 11:26:19 +0200

    init

--- config/bundles.php
+++ config/bundles.php
@@ -2,15 +2,15 @@
 
 return [
     Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
+    Pimcore\Bundle\CustomReportsBundle\PimcoreCustomReportsBundle::class => ['all' => true],
+    Pimcore\Bundle\GlossaryBundle\PimcoreGlossaryBundle::class => ['all' => true],
     Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
     Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
     Pimcore\Bundle\StaticRoutesBundle\PimcoreStaticRoutesBundle::class => ['all' => true],
     Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
     Pimcore\Bundle\UuidBundle\PimcoreUuidBundle::class => ['all' => true],
-    Pimcore\Bundle\XliffBundle\PimcoreXliffBundle::class => ['all' => true],
-    Pimcore\Bundle\CustomReportsBundle\PimcoreCustomReportsBundle::class => ['all' => true],
-    Pimcore\Bundle\GlossaryBundle\PimcoreGlossaryBundle::class => ['all' => true],
     Pimcore\Bundle\WordExportBundle\PimcoreWordExportBundle::class => ['all' => true],
+    Pimcore\Bundle\XliffBundle\PimcoreXliffBundle::class => ['all' => true],
     Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle::class => ['all' => true],
     Pimcore\Bundle\PersonalizationBundle\PimcorePersonalizationBundle::class => ['all' => true],
     Pimcore\Bundle\GoogleMarketingBundle\PimcoreGoogleMarketingBundle::class => ['all' => true],
