8070c406ace5a6f0c357b3f56cab92e441a8e426
Michael Silberbauer <ms@indigo7.de>
2024-06-24 20:36:07 +0200

    bootstrap / ecf

--- src/Controller/Verwaltung/TagsController.php
+++ src/Controller/Verwaltung/TagsController.php
@@ -16,9 +16,48 @@ class TagsController extends BaseController
      */
     #[Route('/verwaltung/tags', name: 'verwaltung_tags')]
     #[IsGranted("IS_AUTHENTICATED")]
-    public function testAction(Request $request): Response
+    public function tagsAction(Request $request): Response
     {
         return $this->render('verwaltung/tags.html.twig');
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/verwaltung/tags-anlegen', name: 'verwaltung_tags-anlegen')]
+    #[IsGranted("IS_AUTHENTICATED")]
+    public function createTagsAction(Request $request): Response
+    {
+        return $this->render('verwaltung/tags_anlegen.html.twig');
+    }
+
+    /**
+     * @param Request $request
+     * @param int $userid
+     * @return Response
+     */
+    #[Route('/verwaltung/tags-bearbeiten/{userid}', name: 'verwaltung_tags-bearbeiten')]
+    #[IsGranted("IS_AUTHENTICATED")]
+    public function editTagsAction(Request $request, int $userid): Response
+    {
+        // dd(\Pimcore\Model\DataObject\Customer::getById($userid));
+        return $this->render('verwaltung/tags_bearbeiten.html.twig');
+    }
+
+    /**
+     * @param Request $request
+     * @param int $userid
+     * @return Response
+     */
+    #[Route('/verwaltung/tags-loeschen/{userid}', name: 'verwaltung_tags-loeschen')]
+    #[IsGranted("IS_AUTHENTICATED")]
+    public function deleteTagsAction(Request $request, int $userid): Response
+    {
+        $user = \Pimcore\Model\DataObject\Customer::getById($userid);
+        // $user->delete();
+
+        return $this->redirectToRoute("verwaltung_tags");
     }
 
 }
