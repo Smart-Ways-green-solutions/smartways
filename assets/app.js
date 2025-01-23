b84ef7a6390a4078e1314adce40c7d77790d4d2f
macmastermike <ms@indigo7.de>
2025-01-23 12:41:29 +0100

    template updates for items

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,10 @@
 import './styles/app.css';
 // import './fonts/fontawesome/css/all.css'
 
+import '@tabler/core/dist/libs/tinymce/skins/ui/oxide/skin.min.css';
+import '@tabler/core/dist/libs/tinymce/skins/ui/oxide/content.min.css';
+import '@tabler/core/dist/libs/tinymce/skins/content/default/content.min.css';
+
 //const $ = require('jquery');
 //window.jQuery = $;
 //window.$ = $;
@@ -15,9 +19,17 @@ import '@tabler/core/dist/js/demo.min';
 import '@tabler/core/dist/js/demo-theme.min';
 import '@tabler/core/dist/libs/fslightbox/index';
 
+import '@tabler/core/dist/libs/tinymce/tinymce.min';
+import '@tabler/core/dist/libs/tinymce/themes/silver/theme.min';
+import '@tabler/core/dist/libs/tinymce/models/dom/model.min';
+import '@tabler/core/dist/libs/tinymce/icons/default/icons.min';
+import '@tabler/core/dist/libs/tinymce/plugins/fullscreen/plugin.min';
+import '@tabler/core/dist/libs/tinymce/plugins/searchreplace/plugin.min';
+import '@tabler/core/dist/libs/tinymce/plugins/advlist/plugin.min';
+import '@tabler/core/dist/libs/tinymce/plugins/insertdatetime/plugin.min';
+
 import '@tabler/core/dist/libs/tom-select/dist/js/tom-select.base';
 import '@tabler/core/dist/libs/nouislider/dist/nouislider';
-
 
 import '@tabler/core/dist/css/tabler.min.css';
 import '@tabler/core/dist/css/demo.min.css';
